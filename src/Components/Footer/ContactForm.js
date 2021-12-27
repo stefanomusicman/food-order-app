54d4cb31f193041edc29626dcc6c9ed1986c91c7
Stefano Proietti <stef.pro@hotmail.com>
2021-12-27 17:20:51 -0500

    email list form updated

--- src/Components/Footer/ContactForm.js
+++ src/Components/Footer/ContactForm.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsTelephone } from 'react-icons/bs';
 import { ImLocation } from 'react-icons/im'
 import classes from './ContactForm.module.css';
 
 const ContactForm = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const nameHandler = (event) => {
+        setName(event.target.value);
+    }
+
+    const emailHandler = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+
+        const formInfo = {
+            name: name,
+            email: email
+        }
+
+        console.log(formInfo);
+
+        setName('');
+        setEmail('');
+    }
+
     return (
         <>
             <div className={classes['footer-top']}>
@@ -12,15 +38,16 @@ const ContactForm = () => {
             </div>
             <div className={classes['main-container']}>
                 <div className={classes['form-container']}>
-                    <form id='clientForm'>
+                    <form onSubmit={formSubmitHandler} id='clientForm'>
                         <div className={classes.name}>
                             <label htmlFor="name">Name</label>
-                            <input id='name' type='text' />
+                            <input onChange={nameHandler} value={name} id='name' type='text' ></input>
                         </div>
                         <div className={classes.phone}>
                             <label htmlFor="email">Email</label>
-                            <input id='email' type='text' />
+                            <input onChange={emailHandler} value={email} id='email' type='text' ></input>
                         </div>
+                        <button className={classes['email-list__submit']} type="submit">Submit</button>
                     </form>
                     {/* <textarea form="clientForm">What can we do for you?</textarea> */}
                 </div>
