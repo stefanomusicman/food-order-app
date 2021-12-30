c2af8a97848b4bc20887f3e7bfe68a6079a16e6e
Stefano Proietti <stef.pro@hotmail.com>
2021-12-30 18:40:00 -0500

    slider is now functioning

--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,38 +1,37 @@
 import React, { useState } from "react";
 import classes from './Slider.module.css';
 
-const reviews = [
-    {
+const reviews = {
+    0: {
         client: 'Anna Johnson',
-        review: 'Absolutely amazing! I always look forward to starting my day with a nice espresso from Cafe Vici!',
+        review: '"Absolutely amazing! I always look forward to starting my day with a nice espresso from Cafe Vici!"',
         id: 1
     },
-    {
+    1: {
         client: 'Giusseppe Saviano',
-        review: 'I am an Italian immigrant and can honestly say that I am impressed with the level of authenticity in everything that Cafe Vici offers.',
+        review: '"I am an Italian immigrant and can honestly say that I am impressed with the level of authenticity in everything that Cafe Vici offers."',
         id: 2
     },
-    {
+    2: {
         client: 'Jack Peters',
-        review: 'Big fan of the Cannolis, goes great with a nice Cappuccino!',
+        review: '"Big fan of the Cannolis, goes great with a nice Cappuccino!"',
         id: 3
     },
-    {
+    3: {
         client: 'Lisa Pietrantonio',
-        review: 'The food is amazing, I\'ve gotten to know the chef Antonino quite well and you can tell how passionate he is about what he does.',
+        review: '"The food is amazing, I\'ve gotten to know the chef Antonino quite well and you can tell how passionate he is about what he does."',
         id: 4
     }
-]
+}
 
 const Slider = () => {
 
     const [current, setCurrent] = useState(reviews[0]);
-    const [active, setActive] = useState(0);
+    const [active, setActive] = useState(false);
 
     const handleSetClick = (event) => {
-        // event.preventDefault();
-        // setCurrent(reviews[event.target.getAttribute('data-quote')]);
-        console.log(event.target.getAttribute('data-quote'));
+        setCurrent(reviews[event.target.getAttribute('data-quote')]);
+        setActive(true)        
     } 
 
     return(
@@ -40,8 +39,8 @@ const Slider = () => {
             <p>{current.review}</p>
             <p>{current.client}</p>
             <div className={classes['dot-container']}>
-                {reviews.map((review) => {
-                    return <span onClick={event => handleSetClick(event)} data-quote={review.index} key={review.id}/>
+                {Object.keys(reviews).map((index) => {
+                    return <span onClick={event => handleSetClick(event)} data-quote={index} key={index}/>
                 })}
             </div>
         </div>
