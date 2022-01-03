f18eed0de453dbf9b884de06a468c210f785987b
Stefano Proietti <stef.pro@hotmail.com>
2022-01-03 17:50:14 -0500

    menu items have been moved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './Components/Header/Header';
@@ -6,61 +6,19 @@ import MenuContainer from './Components/Menu/MenuContainer';
 import Slider from './Components/Slider/Slider';
 import ContactForm from './Components/Footer/ContactForm';
 import Cart from './Components/Cart/Cart';
-import Biscotti from './Images/biscotti.jpg';
-import Cannoli from './Images/cannoli.jpg';
-import Cappuccino from './Images/cappuccino.jpg';
-import Espresso from './Images/espresso.jpg';
-import Focaccia from './Images/focaccia.jpg';
-import Panino from './Images/panino.jpg';
 
-const menuItems = [
-  {
-    name: 'Espresso',
-    price: 2.50,
-    image: <Espresso/>,
-    id: 1
-  },
-  {
-    name: 'Cappuccino',
-    price: 3.50,
-    image: <Cappuccino />,
-    id: 2
-  },
-  {
-    name: 'Focaccia con Mortadella',
-    price: 4.50,
-    image: <Focaccia />,
-    id: 3
-  },
-  {
-    name: 'Cannoli',
-    price: 3.25,
-    image: <Cannoli />,
-    id: 4
-  },
-  {
-    name: 'Panino Bistecca',
-    price: 8.50,
-    image: <Panino />,
-    id: 5
-  },
-  {
-    name: 'Biscotti',
-    price: 2.75,
-    image: <Biscotti />,
-    id: 6
-  }
-];
+
 
 function App() {
+
   return (
     <Router>
       <Switch>
         <Route path='/' exact>
-            <Header />
-            <MenuContainer menu={menuItems}/>
-            <Slider />
-            <ContactForm />
+          <Header />
+          <MenuContainer />
+          <Slider />
+          <ContactForm />
         </Route>
         <Route path='/cart'>
             <Cart />
--- src/Components/Menu/MenuContainer.js
+++ src/Components/Menu/MenuContainer.js
@@ -2,13 +2,58 @@ import React from "react";
 import classes from './MenuContainer.module.css';
 import MenuItemCard from "./MenuItemCard";
 
-const MenuContainer = (props) => {
+import Biscotti from '../../Images/biscotti.jpg';
+import Cannoli from '../../Images/cannoli.jpg';
+import Cappuccino from '../../Images/cappuccino.jpg';
+import Espresso from '../../Images/espresso.jpg';
+import Focaccia from '../../Images/focaccia.jpg';
+import Panino from '../../Images/panino.jpg';
+
+const menuItems = [
+    {
+      name: 'Espresso',
+      price: 2.50,
+      image: <Espresso/>,
+      id: 1
+    },
+    {
+      name: 'Cappuccino',
+      price: 3.50,
+      image: <Cappuccino />,
+      id: 2
+    },
+    {
+      name: 'Focaccia con Mortadella',
+      price: 4.50,
+      image: <Focaccia />,
+      id: 3
+    },
+    {
+      name: 'Cannoli',
+      price: 3.25,
+      image: <Cannoli />,
+      id: 4
+    },
+    {
+      name: 'Panino Bistecca',
+      price: 8.50,
+      image: <Panino />,
+      id: 5
+    },
+    {
+      name: 'Biscotti',
+      price: 2.75,
+      image: <Biscotti />,
+      id: 6
+    }
+  ];
+
+const MenuContainer = () => {
     return(
         <div className={classes['menu-container']}>
             <h1>Menu</h1>
             <div className={classes['items-container']}>
-                {props.menu.map((item) => {
-                    console.log(item.image);
+                {menuItems.map((item) => {
                     return <MenuItemCard name={item.name} price={item.price} image={item.image} key={item.id} />
                 })}
             </div>
