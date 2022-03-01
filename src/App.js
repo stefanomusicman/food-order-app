6aff68667bb8f36c211a32c073c172ddc045b5b0
Stefano Proietti <stef.pro@hotmail.com>
2022-03-01 16:27:06 -0500

    Lazy Loading added on cart and checkout

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Header from './Components/Header/Header';
 import MenuContainer from './Components/Menu/MenuContainer';
 import Slider from './Components/Slider/Slider';
 import ContactForm from './Components/Footer/ContactForm';
-import Cart from './Components/Cart/Cart';
-import CheckoutPage from './Components/Checkout/CheckoutPage';
 import { CartProvider } from './Context/CartContext';
 
-function App() {
-  // create a context for the cart
-  // const [cart, setCart] = React.useState([]);
+const Cart = React.lazy(() => import('./Components/Cart/Cart'));
+const CheckoutPage = React.lazy(() => import('./Components/Checkout/CheckoutPage'));
 
+function App() {
 
   return (
-    // Provider is used to pass data from one component to another, value = cart
     <CartProvider>
       <Router>
-        <Switch>
-          <Route path='/' exact>
-            <Header />
-            <MenuContainer />
-            <Slider />
-            <ContactForm />
-          </Route>
-          <Route path='/cart'>
-            <Cart />
-          </Route>
-          <Route path='/checkout'>
-            <CheckoutPage />
-          </Route>
-        </Switch>
+        <Suspense fallback={<h2>Loading...</h2>}>
+          <Switch>
+            <Route path='/' exact>
+              <Header />
+              <MenuContainer />
+              <Slider />
+              <ContactForm />
+            </Route>
+            <Route path='/cart'>
+              <Cart />
+            </Route>
+            <Route path='/checkout'>
+              <CheckoutPage />
+            </Route>
+          </Switch>
+        </Suspense>
       </Router>
     </CartProvider>
   );
